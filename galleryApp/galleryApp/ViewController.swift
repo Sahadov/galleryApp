dd11464630263b7243b2b000bccef00ffa07e9b1
Dmitry <mrsahadov@gmail.com>
2024-09-19 19:38:24 -0300

    Add the base project

---
+++ galleryApp/galleryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  galleryApp
+//
+//  Created by Дмитрий Волков on 19.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
