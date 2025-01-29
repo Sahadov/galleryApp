6ba177c4c71bc349b91b9a3008dc46af94a9ed9d
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-01-28 21:41:08 -0300

    Change the structure

---
+++ SUI/GalleryAppSUI/GalleryAppSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryAppSUIApp.swift
+//  GalleryAppSUI
+//
+//  Created by Dmitry Volkov on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct GalleryAppSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
